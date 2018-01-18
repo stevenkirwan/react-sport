4b7dd8753db2ff593f80d1b7b75a41da09902e8d
stevenkirwan <kirwan.ste@gmail.com>
2018-01-18 11:04:26 +0000

    league added

--- client/src/components/LeagueItem.js
+++ client/src/components/LeagueItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class LeagueItem extends Component {
 
@@ -8,7 +9,7 @@ export default class LeagueItem extends Component {
                 <div className="card-block">
                     <h4 className="card-title">{this.props.league.strLeague}</h4>
                     <h6 className="card-subtitle mb-2 text-muted">{this.props.league.strSport}</h6>
-                  
+                    {/* <Link to={`/league/${this.props.league.idLeague}`} className="card-link">View table</Link> */}
                 </div>
             </div>
         )
