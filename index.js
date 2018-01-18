328f600ce53364d9e5b6a4283597c4fcbc570810
stevenkirwan <kirwan.ste@gmail.com>
2018-01-18 09:35:39 +0000

    api update

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
+const axios = require('axios');
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
+app.get('/api/leagues', function(req, res) { 
+    axios.get('http://www.thesportsdb.com/api/v1/json/1/all_leagues.php')
+    .then(response => {
+        res.json(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}); 
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -31,4 +27,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
+console.log(`Listening on ${port}`);
